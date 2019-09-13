16d1713fd66cd2ded5d8321b0e5d56aa04049b3b
Vlad <vlad.velichko.qa@gmail.com>
2019-09-13 12:47:48 -0400

    AssertJ Tests added to Save

--- src/test/java/com/rest/qa/AbstractTest.java
+++ src/test/java/com/rest/qa/AbstractTest.java
@@ -1,7 +1,6 @@
 package com.rest.qa;
 
 import io.restassured.RestAssured;
-import org.testng.Reporter;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
@@ -13,17 +12,18 @@ public abstract class AbstractTest {
     static String database;
     static String user;
     static String password;
-    static String endpointAuth;
+    static String authPath;
+    static int retryCount;
 
     @BeforeSuite
-    @Parameters({"serviceUrl", "database", "user", "password"})
-    public void beforeSuite(String serviceUrl, String database, String user, String password) {
+    @Parameters({"serviceUrl", "database", "user", "password", "authPath", "retryCount"})
+    public void beforeSuite(String serviceUrl, String database, String user, String password, String authPath, int retryCount) {
         AbstractTest.serviceUrl = serviceUrl;
         AbstractTest.database = database;
         AbstractTest.user = user;
         AbstractTest.password = password;
-        endpointAuth = Reporter.getCurrentTestResult().getTestContext().getSuite().getXmlSuite().getTests()
-                .stream().filter(t -> t.getName().equals("Authorization")).findFirst().orElseThrow().getParameter("endpoint");
+        AbstractTest.authPath = authPath;
+        AbstractTest.retryCount = retryCount;
 
         RestAssured.baseURI = serviceUrl;
         RestAssured.get();                                              // Check connection.
@@ -32,6 +32,6 @@ public abstract class AbstractTest {
     @BeforeClass
     @Parameters({"endpoint"})
     public void beforeClass(@Optional("") String endpoint) {
-        RestAssured.basePath = endpoint;
+        RestAssured.basePath = endpoint.isBlank() ? authPath : endpoint;
     }
 }
--- src/test/java/com/rest/qa/Save.java
+++ src/test/java/com/rest/qa/Save.java
@@ -1,18 +1,48 @@
 package com.rest.qa;
 
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.when;
+import static java.lang.Thread.sleep;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Save extends AbstractTest {
 
+    @BeforeClass
+    public void authorize() {
+        SaveRequest.setUser(user);
+        SaveRequest.setPassword(password);
+        SaveRequest.setAuthPath(authPath);
+        SaveRequest.setDefaultRetryCount(retryCount);
+    }
+
     @Test(description = "Saving data without token should return error code 403. No new records in database")
-    public void testSaveWOToken() {
-        when()
-                .post()
-                .then()
-                .statusCode(403)
-                .and()
-                .assertThat();
+    public void testSaveWithoutToken() {
+        when().post().then().statusCode(403);
+        //todo: check No new records in database
+    }
+
+    @Test(description = "Saving data with outdated token should return error code 403. No new records in database")
+    public void testSaveWithOutdatedToken() throws InterruptedException {
+        new SaveRequest().authorize();
+        System.out.println("Waiting 61 second for token expiration..."); //Todo: need logging
+        sleep(61_000);
+        Response response = new SaveRequest()
+                .payload("any payload")
+                .autoAuth(false)
+                .post();
+        assertThat(response.statusCode()).isEqualTo(403);
+        //todo: check No new records in database
+    }
+
+    @Test(description = "Saving data with wrong payload should return error code 400. No new records in database")
+    public void testSaveWrongPayload() {
+        Response response = new SaveRequest()
+                .payload("Wrong Raw Payload")
+                .post(0);
+        assertThat(response.statusCode()).isEqualTo(400);
+        //todo: check No new records in database
     }
 }
